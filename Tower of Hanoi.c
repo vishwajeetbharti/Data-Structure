41fcee9a1c2030b76a27191f394e505898af7f46
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2022-03-19 23:39:46 +0530

    Create Tower of Hanoi.c
    Tower of Hanoi is a mathematical game or puzzle....

---
+++ Tower of Hanoi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+Tower(int k, char frompeg,char midpeg, char topeg)
+{
+	if(k==1)
+	{
+		printf("Move the disk 1 from peg %c to peg %c",frompeg,topeg);
+		return; 
+	}
+	Tower(k-1,frompeg,midpeg,topeg);
+	printf("\nMove disk %d from peg %c to peg %c\n", k, frompeg, topeg);
+	Tower(k-1,midpeg,topeg,frompeg);
+}
+void main()
+{
+	int a;
+	printf("Enter the number of disk...\n");
+	scanf("%d",&a);
+	printf("The moves involved in the Tower of Hanoi are :\n");
+	Tower(a,'A','C','B');
+}
